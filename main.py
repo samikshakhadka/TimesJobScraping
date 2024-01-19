4b1432ded0754d52bdc1eb5cf39d0c8568bef27f
Samiksha <swabhimankhadka2001@gmail.com>
2024-01-19 17:05:57 +0545

    feat(storing data): filtered out jobs stored in .txt file

--- main.py
+++ main.py
@@ -7,29 +7,33 @@ def get_jobs(url):
     jobs = soup.find_all("li", class_="clearfix job-bx wht-shd-bx")
     return jobs
 
-def print_job_details(job):
-    company_name = job.find("h3", class_="joblist-comp-name").text.strip()
-    skills = job.find("span", class_="srp-skills").text.strip()
-    more_info = job.find("h2").a["href"]
 
-    print(f"Company Name: {company_name}")
-    print(f"Skills Required: {skills}")
-    print(f"More Info: {more_info}")
-    print()
 
 def main():
+    def print_job_details(job, index):
+        company_name = job.find("h3", class_="joblist-comp-name").text.strip()
+        skills = job.find("span", class_="srp-skills").text.strip()
+        more_info = job.find("h2").a["href"]
+
+        with open(f'post/{index}.txt', 'w') as f:
+            f.write(f"Company Name: {company_name}\n")
+            f.write(f"Skills Required: {skills}\n")
+            f.write(f"More Info: {more_info}\n")
+        print(f"file saved: {index}")
+
+
     base_url = "https://www.timesjobs.com/candidate/job-search.html"
     url_params = "?searchType=personalizedSearch&from=submit&searchTextSrc=&searchTextText=&txtKeywords=python&txtLocation="
     url = base_url + url_params
     jobs = get_jobs(url)
 
-    for job in jobs:
+    for index, job in enumerate(jobs):
         job_published_date = job.find("span", class_="sim-posted").text
         skills = job.find("span", class_="srp-skills").text.strip()
 
         if 'few' in job_published_date and (
                 operating_choice == '1' and unfamiliar_skills not in skills) or operating_choice == '2':
-            print_job_details(job)
+            print_job_details(job, index)
 
 
 if __name__ == "__main__":

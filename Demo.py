3dec60995ce7f7825650df708ae5d119f752d2aa
Samiksha <swabhimankhadka2001@gmail.com>
2024-01-19 14:23:50 +0545

    feat(job published date) : filter out jobs posted few days ago

--- Demo.py
+++
@@ -1,7 +0,0 @@
-from bs4 import BeautifulSoup
-import requests
-
-html_text = requests.get("https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&searchTextSrc=&searchTextText=&txtKeywords=python&txtLocation=").text
-soup = BeautifulSoup(html_text, "lxml")
-jobs = soup.find_all("li", class_="clearfix job-bx wht-shd-bx")
-print(jobs)
